62f5b6db6dc33371b90b994ac636b3f1dcf885c7
Alexei <aliakseiravenka@gmail.ru>
2022-11-02 13:09:01 +0300

    Question about parameters

--- cypress/tests/Task_10_Converter.js
+++ cypress/tests/Task_10_Converter.js
@@ -6,13 +6,13 @@ import Chance from 'chance'
 
 
 describe("Converter e2e test", () => {
-    before(function () {
+    beforeEach(function () {
         cy.fixture('currency').then(currensy => {
             this.currency = currensy;                        // now 'this.currency' visible for tests BUT ARROW FUNCTIONS DO NOT BE EXIST in 'before()' 
         });                                                  // and 'it()' in this case  https://docs.cypress.io/api/commands/fixture#this-context
     });
 
-    it("Task 10", function () {
+    it("Task 10 one", function () {
         let targetCurrency = Chance().pickone(this.currency.rates);
 
         cy.log('GIVEN User is at Main Page:');
@@ -28,5 +28,25 @@ describe("Converter e2e test", () => {
         ConvertPage.getRateInfo.then(rateInfo => {
             expect(parseFloat(rateInfo.text())).to.equal(targetCurrency.rate)      // test always fail because our fixture is static but the rate at the
         })                                                                         // site is dynamic
+    });
+
+
+    it("Task 10 scope", function () {
+        let scopeTargetCurrency = this.currency.rates;
+        scopeTargetCurrency.forEach((targetCurrency) => {
+            cy.log('GIVEN User is at Main Page:');
+            MainPage.open();
+
+            cy.log('WHEN User selects needed currencies:')
+            MainPage.getInputAmount.click().clear().type(1);
+            MainPage.getSelectFrom.type(this.currency.base).type(`{enter}`);
+            MainPage.getSelectTo.type(targetCurrency.shortName).type(`{enter}`);
+            MainPage.getButtonConvert.click();
+
+            cy.log('THEN User can see currencies convert rate at Convert Page:')
+            ConvertPage.getRateInfo.then(rateInfo => {
+                expect(parseFloat(rateInfo.text())).to.equal(targetCurrency.rate)      // test always fail because our fixture is static but the rate at the site is dynamic
+            })
+        })
     });
 });
